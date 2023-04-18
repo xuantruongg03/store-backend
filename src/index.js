c0df55e0a0478b3ac3833ae5af736a12f1709d3c
xuantruongg03 <108038337+xuantruongg03@users.noreply.github.com>
2023-04-18 20:37:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const path = require("path");
 const morgan = require("morgan");
 require("dotenv").config();
 const handlebars = require("express-handlebars");
-const initWebRoute = require("./src/resources/route/web");
-const initAPIWebRoute = require("./src/resources/route/api");
+const initWebRoute = require("./resources/route/web");
+const initAPIWebRoute = require("./resources/route/api");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //use morgan for console log
 app.use(morgan("combined"));
@@ -41,7 +41,7 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, ".src//resources/views"));
+app.set("views", path.join(__dirname, "./resources/views"));
 
 initWebRoute(app);
 initAPIWebRoute(app);

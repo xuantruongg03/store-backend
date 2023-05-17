bae927293ab90e6bcaa31897c03a12aee1b84716
xuantruongg03 <108038337+xuantruongg03@users.noreply.github.com>
2023-05-17 14:46:09 +0700

    update

--- src/resources/controller/web/Blog.js
+++ src/resources/controller/web/Blog.js
@@ -7,14 +7,14 @@ const getCensoring = async (req, res) => {
 
 const getCensorred = async (req, res) => {
     const [rows, fields] = await pool.execute(`select blog_id, blog_title, first_name, last_name, blog_description from blogs inner join customers on customers.customer_id = blogs.customer_id where blog_status = 1`)
-    return res.render("blogCensoring", {data: rows});
+    return res.render("blogCensorred", {data: rows});
 }
 
 const deleteBlog = async (req, res) => {
     const id = req.params.id;
     try {
         await pool.execute (`delete from blogs where blog_id = ${id}`);
-        return res.redirect("/cms");
+        return res.redirect("/cms/get-censorred");
     } catch (error) {
         console.log(error);
     }
